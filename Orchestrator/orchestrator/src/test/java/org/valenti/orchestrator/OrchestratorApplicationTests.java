ff9a640531bf20372b2bc5ecf5789531494dce20
Alessandro Bellia <casilda.bell85@gmail.com>
2023-07-19 00:46:59 +0200

    k8s and fixes
    Signed-off-by: Alessandro Bellia <casilda.bell85@gmail.com>

---
+++ Orchestrator/orchestrator/src/test/java/org/valenti/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.valenti.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
